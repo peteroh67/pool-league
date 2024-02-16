b1ee215a18244354d9503bab282f589e9c2f7722
Peter O'Hare <peteroh500@gmail.com>
2024-02-16 19:22:18 +0000

    Create data models and migrate

--- mysite/pool/models.py
+++ mysite/pool/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    games_played = models.IntegerField(default=0)
+    games_won = models.IntegerField(default=0)
+class Game(models.Model):
+    player_1 = models.ForeignKey(Player, related_name='games_as_player_1', on_delete=models.CASCADE)
+    player_2 = models.ForeignKey(Player, related_name='games_as_player_2', on_delete=models.CASCADE)
+    winner = models.ForeignKey(Player, related_name='won_games', on_delete=models.CASCADE)
+    game_date = timezone.now().date()
+
+
