cfeb1d7946b9c49139bbdc46bc67fd7b3c166549
Peter O'Hare <peteroh500@gmail.com>
2024-02-16 20:39:57 +0000

    Refactor models with many to many relationship

--- mysite/pool/models.py
+++ mysite/pool/models.py
@@ -1,15 +1,19 @@
 from django.db import models
-from django.utils import timezone
 
 class Player(models.Model):
     name = models.CharField(max_length=200)
-    games_played = models.IntegerField(default=0)
-    games_won = models.IntegerField(default=0)
+    number_of_games = models.IntegerField(default=0)
+    number_of_games_won = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 class Game(models.Model):
-    player_1 = models.ForeignKey(Player, related_name='games_as_player_1', on_delete=models.CASCADE)
-    player_2 = models.ForeignKey(Player, related_name='games_as_player_2', on_delete=models.CASCADE)
+    players = models.ManyToManyField(Player, related_name='games_played')
     winner = models.ForeignKey(Player, related_name='won_games', on_delete=models.CASCADE)
     game_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.players.all()[0]} vs {self.players.all()[1]} - Winner: {self.winner}"
 
 
