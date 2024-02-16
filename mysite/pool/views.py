ec3c05131be35ee3187773469452101b0d284348
Peter O'Hare <peteroh500@gmail.com>
2024-02-16 21:19:54 +0000

    Add new views for add game and view results

--- mysite/pool/views.py
+++ mysite/pool/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello world. This is the pool index')
+
+def add_game(request):
+    return HttpResponse("Add a game of pool")
+
+def results(request, start_date, end_date):
+    return HttpResponse("Viewing results")
