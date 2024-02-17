15ad3355c2dd05ce514a8498d89a0cc7cd2cbfb0
Peter O'Hare <peteroh500@gmail.com>
2024-02-17 11:16:35 +0000

    Remove redundant fields from player class

--- mysite/pool/models.py
+++ mysite/pool/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 class Player(models.Model):
     name = models.CharField(max_length=200)
-    number_of_games = models.IntegerField(default=0)
-    number_of_games_won = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
