89b5e9058db2c0df16806bcb562c27f6df040585
Peter O'Hare <peteroh500@gmail.com>
2024-02-17 12:23:38 +0000

    change game class players from many-to-many to two separate foreign key

--- mysite/pool/models.py
+++ mysite/pool/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
+
 class Player(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
 class Game(models.Model):
-    players = models.ManyToManyField(Player, related_name='games_played')
+    player_1 = models.ForeignKey(Player, related_name='games_as_player_1', on_delete=models.CASCADE)
+    player_2 = models.ForeignKey(Player, related_name='games_as_player_2', on_delete=models.CASCADE)
     winner = models.ForeignKey(Player, related_name='won_games', on_delete=models.CASCADE)
     game_date = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.players.all()[0]} vs {self.players.all()[1]} - Winner: {self.winner}"
-
-
+        return f'{self.player_1.name} vs {self.player_2.name} - Winner: {self.winner.name}'
